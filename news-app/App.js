d39b64006b940677678243ce2a2b1acb3e381e99
Edanker <eledyall@bk.ru>
2023-05-23 22:52:07 +0500

    init_Progect

--- news-app/App.js
+++ news-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View,} from 'react-native';
 export default function App() {
   return (
     <View  style={styles.container}>
-      <Text>Hello Muha</Text>
+      <Text>ты хуло</Text>
       <StatusBar style="auto" />
     </View>
   );
