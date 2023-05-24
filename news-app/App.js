a9c730c512f506059ba10c41c1b5bf08d0d4a9f2
Edanker <eledyall@bk.ru>
2023-05-25 03:17:46 +0500

    Registration

--- news-app/App.js
+++ news-app/App.js
@@ -1,35 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, SafeAreaView, StyleSheet} from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Image, onChangeText, Button, ButtonProps} from 'react-native';
 
 
 export default function App() {
   return (
     <View  style={styles.container}>
-      <StatusBar style="auto" />
+        <View style={{}}>
+          <Text style={{marginTop: 80, fontSize: 20, marginLeft: 39, fontWeight: 'bold'}}>Добро пожаловать в voice_ithab</Text>
+          <TextInput maxLength={40} style={{marginTop: 150, width: 200, height: 50, borderBottomColor: 'black', borderBottomWidth: 1, marginLeft: 80 }} onChangeText={onChangeText} value={Text} placeholder="Id-college"></TextInput>
+          <TextInput maxLength={40} style={{marginTop: 0, width: 200, height: 50, borderBottomColor: 'black', borderBottomWidth: 1, marginLeft: 80 }} onChangeText={onChangeText} value={Text} placeholder="password"></TextInput>
+        </View>
+        <Image source={require('./assets/pngwing.com.png')} style={{width: 40, height: 40, marginLeft: 10, marginTop: 200}}></Image>  
     </View>
   );
 }
 
-const TextInputExempel = () => {
-    const [text, onChamgeText] = React.useState('Id');
-
-    return (
-      <SafeAreaView>
-        <TextInput>
-          style={styles.input}
-          onChamgeText={onChamgeText}
-          value={text}
-        </TextInput>
-      </SafeAreaView>
-    );
-};
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: 'white',
+      },
+    });
